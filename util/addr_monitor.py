c43ce4b54068dfed82e72306f7f84e147c051a0e
zwyang <yangziwei@me.com>
2020-10-05 22:18:13 +0800

    addr_monitor added

---
+++ util/addr_monitor.py
@@ -0,0 +1,25 @@
+import os
+import sys
+import json
+from web3 import Web3
+from eth_tool.common import web3_eth, cache, etherscan, logger
+from eth_tool.common.logger import debug, log
+
+ROOT_DIR = os.environ['ETH_TOOLS_DIR']
+contacts_ = ROOT_DIR + '/data/contacts.json'
+if len(sys.argv) >= 2:
+    contacts_ = sys.argv[1]
+
+log("Loading", contacts_)
+with open(contacts_, 'r') as f:
+    contacts = json.loads(f.read())
+
+for alias in contacts:
+    addr_list = contacts[alias]
+    for addr in addr_list:
+        addr = Web3.toChecksumAddress(addr)
+        debug("Scanning", alias, addr)
+        new_txs = etherscan.addr_tx_update(addr, verbose=True)
+        for tx in new_txs:
+            log("New TX", alias, addr)
+            print(etherscan.format_tx(tx, addr=addr))
